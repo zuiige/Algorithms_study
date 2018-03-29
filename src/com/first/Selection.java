adcad988fe30fc1390f54efff122cad482b7fc99
zuiige <2569290395@qq.com>
2018-03-29 23:18:03 +0800

    排序

---
+++ src/com/first/Selection.java
@@ -0,0 +1,21 @@
+package com.first;
+
+import java.util.List;
+
+public class Selection {
+
+	public static void sort(List[] a){
+		int N = a.length;
+		for(int i = 0 ; i < N ; i++){
+			int min = i;
+			for(int j = i + i; j< N;j++){
+				
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
